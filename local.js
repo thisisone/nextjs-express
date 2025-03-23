350b895fb1a5cc842111e94a905aefae80f9eef5
gunil <this_is_one@naver.com>
2025-03-24 05:26:05 +0900

    v21

--- local.js
+++ local.js
@@ -1,4 +1,9 @@
 // local.js
 // 로컬에서 테스트할때 쓴다.
-require("./server");
+const app = require("./server");
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
+
 console.log("local server start");
