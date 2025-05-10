ec8f3a1f5e78598450ecef487d4d3ad31daecb2f
gunil <this_is_one@naver.com>
2025-05-10 15:51:44 +0900

    dev

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require("express");
-const { set_root_dir, proc_all_file } = require("./dist/src/express");
-set_root_dir(__dirname);
+// const { set_root_dir, proc_all_file } = require("./dist/src/express");
+// set_root_dir(__dirname);
 
 const app = express();
 
-app.get("/", proc_all_file);
-app.get("/style.css", proc_all_file);
-app.get("/favicon.ico", proc_all_file);
-app.get("/favicon.png", proc_all_file);
-app.get("/webgl*", proc_all_file);
-
-app.get("/api/**", (req, res) => {
-  console.log("get /abc", req.url);
-  res.send("ok");
+let count = 0;
+app.get("/", (req, res) => {
+  console.log("GET /", req.url);
+  res.send("ok, " + count++);
 });
+// app.get("/", proc_all_file);
+// app.get("/style.css", proc_all_file);
+// app.get("/favicon.ico", proc_all_file);
+// app.get("/favicon.png", proc_all_file);
+// app.get("/webgl*", proc_all_file);
+
+// app.get("/api/**", (req, res) => {
+//   console.log("get /abc", req.url);
+//   res.send("ok");
+// });
 
 // 서버 시작
 let PORT = 3000;
@@ -25,8 +30,8 @@ console.log("PORT", PORT);
 app.listen(PORT, () => {
   console.log("express listen");
   console.log(`http://localhost:${PORT}/`);
-  console.log(`http://localhost:${PORT}/api`);
-  console.log("http://localhost:3000/webgl_mp/index.html");
+  console.log(`https://unity.sidnft.com`);
+  console.log(`https://unity.sidnft.com/webgl_mp/index.html`);
 });
 
 module.exports = app;
