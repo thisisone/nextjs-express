b629d015aa2f6a92d88b9fae12f01de8ceafba9d
gunil <this_is_one@naver.com>
2025-05-10 19:21:08 +0900

    prod

--- dist/src/ver.js
+++ dist/src/ver.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BUILD_DATE = exports.VERSION = void 0;
-exports.VERSION = 27;
-exports.BUILD_DATE = '2025-05-10 19:08:11';
+exports.VERSION = 28;
+exports.BUILD_DATE = '2025-05-10 19:21:02';
--- src/ver.ts
+++ src/ver.ts
@@ -1,2 +1,2 @@
-export const VERSION=27;
-export const BUILD_DATE='2025-05-10 19:08:11';
+export const VERSION=28;
+export const BUILD_DATE='2025-05-10 19:21:02';
